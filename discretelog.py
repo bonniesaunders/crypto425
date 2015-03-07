4597d42319dc568c95d85110acd1ffa549f8d57c
Bonnie Saunders <19861739+bonniesaunders@users.noreply.github.com>
2015-03-07 08:28:08 -0600

    last year's version has been update to include latest updates to matmod, which now has a __hash__.

--- discretelog.py
+++ discretelog.py
@@ -1,4 +1,4 @@
-from matmodtest import *
+from matmod import *
 
 def d_log(alpha,beta,p):
     """
@@ -6,7 +6,12 @@ alpha and beta are mod p numbers. p is pari integer.
 returns solution, x, to alpha**x = beta mod p
     """
     N = (p-1).sqrt().floor() + 1
-    list_one = { alpha**j:j for j in xrange(N) }
+#    list_one = { alpha**j:j for j in xrange(N) }
+    list_one = {}
+    y = 1 |mod| p
+    for j in xrange(N):
+        list_one[y]=j
+        y = y*alpha
 #    print list_one
 #    print 'N={}'.format(N)
     for k in xrange(N):
