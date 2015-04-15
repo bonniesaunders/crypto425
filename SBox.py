80dc4eb82faf4c691b189f5dd5e09d04d767e8ef
Bonnie Saunders <19861739+bonniesaunders@users.noreply.github.com>
2015-04-15 11:13:12 -0500

    finished.

--- SBox.py
+++ SBox.py
@@ -14,6 +14,7 @@ p = Poly(0b100011011)
 
 CV = [[1],[1],[0],[0],[0],[1],[1],[0]] |mod| 2
 
+
 def reverse(L):
     return [ x for x in reversed(L) ]
 
@@ -22,4 +23,18 @@ def row(L):
 
 def column(L):
     return [ [x] for x in L ] 
-    
+
+a = Poly(0b11111)
+c = Poly(0b1100011)
+mod = rijndael_poly()    
+mod8 = Poly(0b100000001)
+
+def q(x):
+    return (a*x+c).MOD(mod8)
+
+def SBox():
+    for r in range(16):
+        if r == 0: print [ q(0).hex]+[ q( Poly(r*16 + b).inverse(mod) ).hex.zfill(2) for b in range(1,16)]
+        else: print [ q( Poly(r*16 + b).inverse(mod) ).hex.zfill(2) for b in range(16)]
+    return 'done'
+            
